2db073cbc42440b8d2fddc91f9bc020713b1ff49
Kasle <aleks.mercik@outlook.com>
2015-12-09 00:41:52 -0500

    Wat

--- HWK6/ArithmeticExpression.h
+++ HWK6/ArithmeticExpression.h
@@ -2,32 +2,20 @@
 #define ARITHMETICEXPRESSION_H_
 
 #include "Expression.h"
+
 #include <string>
 #include <stdexcept>
 
 using namespace std;
 
-class Multiply;
-class Divide;
-class Add;
-class Subtract;
-
-class ArithmeticExpression : public Expression {
+class ArithmeticExpression: public Expression{
+    Expression *left;
+    Expression *right;
 
     public:
-        Expression *left;
-        Expression *right;
-
-        ArithmeticExpression();
-        ArithmeticExpression(string);
-        bool parse();
-        //string evaluate(); //evaluate left expression and right expression
-        void print();
-        float convert (string);
-        virtual ~ArithmeticExpression();
-    private:
-        void parseBrackets();
-        void parseExpression(char);
-        bool checkCharIs(char, string);
+        string evaluate(){};
+        void print(){};
+        float convert(string){};
 };
+
 #endif
