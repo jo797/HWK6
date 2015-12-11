efad21274c741ff66255822b2cccb49865fd9492
Kasle <aleks.mercik@outlook.com>
2015-12-11 13:44:43 -0500

    Commented Add, Sub, Mult, Div. Classes

--- HWK6/Add.cpp
+++ HWK6/Add.cpp
@@ -1,21 +1,21 @@
 #include "Add.h"
 
-string Add::evaluate(){
-    if (left != NULL && right != NULL){
-        return to_string(convert(left->evaluate()) + convert(right->evaluate()));
-    } else {
-        return exp;
+string Add::evaluate(){ // evaluation function
+    if (left != NULL && right != NULL){ // If the left and right expressions are not empty
+        return to_string(convert(left->evaluate()) + convert(right->evaluate())); // return this objects evaluation ie. (x + y) for addition. calls evaluation for left and right objects.
+    } else { // if the evaluation is a value
+        return exp; // return just the value
     }
 }
 
-void Add::print(){
-    if (left != NULL && right != NULL){
-        cout << "(";
-        left->print();
-        cout << "+";
-        right->print();
-        cout << ")";
+void Add::print(){ // print function
+    if (left != NULL && right != NULL){ // If something points to the left and right, ie. it is not a value
+        cout << "("; // Print outside brackets
+        left->print(); // print the left expression, calling this function from the left expression
+        cout << "+"; // Addition, so + sign
+        right->print(); // call print for the right expression
+        cout << ")"; // print out a closing bracket
     } else {
-        cout << exp;
+        cout << exp; // print out the expression, ie. the value.
     }
 }
